950ade8232702c7246882dddc304747b830bb176
David Tovar Goris <sgoris@gmail.com>
2013-09-24 00:03:00 -0500

    Adding some missing columns to exam definitions

---
+++ db/migrate/20130924043958_add_start_date_to_exams.rb
@@ -0,0 +1,10 @@
+class AddStartDateToExams < ActiveRecord::Migration
+  def change
+	add_column :exams, :startdate, :date
+	add_column :master_exams, :startdate, :date
+	add_column :exams, :finishdate, :date
+	add_column :master_exams, :finishdate, :date
+
+
+  end
+end
