cc6b58a242e4d0ce15ff05c39cfbdb98bb258b1f
David <sgoris@gmail.com>
2013-09-24 00:40:27 -0500

    Update 20130924043958_add_start_date_to_exams.rb

--- db/migrate/20130924043958_add_start_date_to_exams.rb
+++ db/migrate/20130924043958_add_start_date_to_exams.rb
@@ -1,6 +1,5 @@
 class AddStartDateToExams < ActiveRecord::Migration
   def change
-	add_column :exams, :startdate, :date
 	add_column :master_exams, :startdate, :date
 	add_column :exams, :finishdate, :date
 	add_column :master_exams, :finishdate, :date
