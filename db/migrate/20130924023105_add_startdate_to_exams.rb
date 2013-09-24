ffc89030990a8077be861ff93fbf24acc4fb22c8
David Tovar Goris <sgoris@gmail.com>
2013-09-23 22:34:52 -0500

    Add startddate to master examens

--- db/migrate/20130924023105_add_startdate_to_exams.rb
+++ db/migrate/20130924023105_add_startdate_to_exams.rb
@@ -2,6 +2,6 @@ class AddStartdateToExams < ActiveRecord::Migration
   def change
 	  add_column :exams, :startdate, :date
 	  add_column :exams, :finishdate, :date
-	
+	  add_column :master_exams, :finishdate, :date
   end
 end
