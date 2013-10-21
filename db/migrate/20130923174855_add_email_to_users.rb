94d47b1c1c2512bb41248dbb235cbf46ab49242e
raulgomez <raulg@Joses-MacBook-Pro-4.local>
2013-10-21 13:24:13 -0500

    Mostrar Preguntar

--- db/migrate/20130923174855_add_email_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailToUsers < ActiveRecord::Migration
-  def change
-  	
-  	add_column :users , :mail, :string
-  end
-end
--- db/migrate/20130923204408_add_mail_to_users.rb
+++
@@ -1,6 +0,0 @@
-
-class AddMailToUsers < ActiveRecord::Migration
-  def change
-
-  end
-end
