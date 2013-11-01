3ec7cec9e8b634ca192cb83d413df48cefc8820c
julia <rms.julle@gmail.com>
2013-11-01 14:55:29 -0600

    mas email

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -4,10 +4,10 @@ class EmailController < ApplicationController
       email = @params["email"]
 	  recipient = "rms.julle@gmail.com"
 	  subject = "codea feliz"
-	  message = "Prueba del correo de codigo feliz"
+	  message = "Gracias por presentar el examen, puedes revisar tus resultados en http://codeafeliz.com/exams/25"
       UserEmail.deliver_contact(recipient, subject, message)
       return if request.xhr?
       render :text => 'Se mando exitosamente'
-      redirect_to(root_path)
+      redirect_to(pending_path)
    end
 end
